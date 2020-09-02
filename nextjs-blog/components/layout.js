aed96a3695013e9c6bb307245086c20d7c126392
huynvn <hpn010193@gmail.com>
2020-09-02 15:14:48 +0700

    Update code

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import styles from './layout.module.css';
+import utilStyles from '../styles/utils.modules.css';
 import Link from 'next/link';
 
 const name = 'Your name';
@@ -9,7 +11,41 @@ export default function Layout({ children, home }) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.icon" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(siteTile)}.png?theme=light&md=0&fontSize=75px&image=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTile} />
+        <meta name="twitter:card" content="sumary_large_image" />
       </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+            <>
+              <Link href="/">
+                <a>
+                  <img
+                    src="/images/profile.jpg"
+                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                    alt={name}
+                  />
+                </a>
+              </Link>
+            </>
+          )}
+      </header>
     </div>
   )
 }
